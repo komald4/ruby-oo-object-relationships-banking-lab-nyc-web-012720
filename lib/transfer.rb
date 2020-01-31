0a944226dadaa661aba81142923d0ee5695de2ad
Komal Dindiyal <komald.4@gmail.com>
2020-01-31 16:47:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,9 +28,9 @@ class Transfer
 
    def reverse_transfer
      if status == "complete"
-       @sender.balance -= @amount
-       @receiver.balance += @amount
-       @status = "complete"
+       @sender.balance += @amount
+       @receiver.balance -= @amount
+       @status = "reversed"
      end
 
    end
