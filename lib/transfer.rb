230d45f84f749956d938640c454da1fc8a87b949
Komal Dindiyal <komald.4@gmail.com>
2020-01-31 16:18:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-     if self.valid? == false || status == "Reject"
+     if self.valid? == true || status == "complete"
        @status = "rejected"
        return "Transaction rejected. Please check your account balance."
      else
