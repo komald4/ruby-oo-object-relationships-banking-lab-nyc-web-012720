be60a3895719db9af39fc42bb24ab967b51cb2cf
Komal Dindiyal <komald.4@gmail.com>
2020-01-31 15:38:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,10 @@
 class Transfer
-  attr_accessor :sender, :receiver, :transfer_amount
-  def initialize(sender, receiver, transfer_amount)
+  attr_accessor :sender, :receiver, :amount
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
-    @transfer_amount = transfer_amount
+    @amount = amount
   end
 
 
