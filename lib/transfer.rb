6a6578f7a6ecbe23f19f5bbae0814f6e93dc90cb
Komal Dindiyal <komald.4@gmail.com>
2020-01-31 15:35:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
   attr_accessor :sender, :reciever, :status
   def initialize(sender, reciever, status)
-    @sender
-    @reciever
-    @status
+    @sender = sender
+    @reciever = reciever
+    @status = status
   end
 
 
